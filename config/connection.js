e5375e53b8db725b135b1a5afff4c5acd02e0f15
Felix Ruano <felixruano.91@gmail.com>
2018-11-24 18:21:50 -0600

    That small update sucked

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
 }
 else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "127.0.0.1",
     user: "root",
     password: "",
     database: "burgers_db"
