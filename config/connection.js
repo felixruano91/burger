2c67a4c4676c04b6e20cfc3e9961d2d034ffe390
Felix Ruano <felixruano.91@gmail.com>
2018-12-07 23:21:20 -0600

    Heroku updates

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,8 @@ if (process.env.JAWSDB_URL) {
 }
 else {
   connection = mysql.createConnection({
-    host: "127.0.0.1",
+    host: "localhost",
+    port: 3306,
     user: "root",
     password: "",
     database: "burgers_db"
